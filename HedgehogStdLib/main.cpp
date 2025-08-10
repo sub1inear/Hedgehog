f37c2a5bc37366c8d3c6d8320b98155541ed21f4
unknown <odoul28@gmail.com>
2025-08-10 09:57:48 -0700

    Move h_char implementation to header file

--- HedgehogStdLib/main.cpp
+++ HedgehogStdLib/main.cpp
@@ -4,6 +4,5 @@
 int main() {
     h_str str = "This is an interesting example to show how to use the find and replace mechanisms to do things.";
     println(str);
-    str.replace_reverse("to", "two");
     print(str);
 }
