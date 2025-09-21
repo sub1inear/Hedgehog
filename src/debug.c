e590f30f94dc5fdc544b922ba529b9e287e03bae
unknown <odoul28@gmail.com>
2025-09-21 06:38:13 -0700

    Separate debug functions from main.c

---
+++ src/debug.c
@@ -0,0 +1,38 @@
+#include "debug.h"
+#include "lexer.h"
+#include "token.h"
+#include "parser.h"
+#include "node.h"
+
+void hhg_debug_lexer(const char *filename)
+{
+    hhg_lexer_t lexer;
+    hhg_lexer_init(&lexer, filename);
+    
+    while (lexer.token.type != EOF) {
+        hhg_lexer_next(&lexer);
+        hhg_token_print(&lexer.token);
+        putchar('\n');
+    }
+
+    hhg_lexer_del(&lexer);
+}
+
+void hhg_debug_parser(const char *filename)
+{
+    hhg_lexer_t lexer;
+    hhg_lexer_init(&lexer, filename);
+
+    hhg_node_t *program = hhg_parse(&lexer);
+
+    if (hhg_msgs_has_errors()) {
+        hhg_msgs_print();
+        hhg_msgs_del();
+
+        hhg_lexer_del(&lexer);
+        exit(1);
+    }
+
+    hhg_node_print(program, HHG_NODE_INDENT_START);
+    hhg_lexer_del(&lexer);
+}
--- src/main.c
+++ src/main.c
@@ -1,46 +1,9 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#include "lexer.h"
-#include "parser.h"
-#include "error.h"
-#include "sym.h"
-#include "mem.h"
-
-static void hhg_test_lexer(const char *filename)
-{
-    hhg_lexer_t lexer;
-    hhg_lexer_init(&lexer, filename);
-    
-    while (lexer.token.type != EOF) {
-        hhg_lexer_next(&lexer);
-        hhg_token_print(&lexer.token);
-        putchar('\n');
-    }
-
-    hhg_lexer_del(&lexer);
-}
-
-static void hhg_test_parser(const char *filename)
-{
-    hhg_lexer_t lexer;
-    hhg_lexer_init(&lexer, filename);
-
-    hhg_node_t *program = hhg_parse(&lexer);
-
-    if (hhg_msgs_has_errors()) {
-        hhg_msgs_print();
-        hhg_msgs_del();
-
-        hhg_lexer_del(&lexer);
-        exit(1);
-    }
-
-    hhg_node_print(program, HHG_NODE_INDENT_START);
-    hhg_lexer_del(&lexer);
-}
-
+#include "debug.h"
 int main(void)
 {
+    hhg_debug_parser("../test/testcases/float.hhg");
     return 0;
 }
