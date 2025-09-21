e6d5704a2e17fb39ee2981dd4d1ab525bf7d3e9b
unknown <odoul28@gmail.com>
2025-09-21 07:49:11 -0700

    Color coat hhg_unit_assert failures

--- test/include/unit.h
+++ test/include/unit.h
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #define hhg_unit_assert(test) if (!(test)) return 1;
-#define hhg_unit_assert_msg(test, msg) do { if (!(test)) { \
-                                                fputs(msg, stderr); \
-                                                return 1; \
-                                            } \
-                                       } while (0)
+#define hhg_unit_assert_msg(test, msg) \
+            do { if (!(test)) { \
+                    fputs("\x1b[1;31m" "hhg_unit_assert failed: " \
+                          msg "\n" "\x1b[0m", stderr); \
+                    return 1; \
+                } \
+            } while (0)
