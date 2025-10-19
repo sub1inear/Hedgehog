493848232aec6af0e5891c81995835c2d765d1f8
unknown <odoul28@gmail.com>
2025-10-19 10:43:10 -0700

    Standardize program name detection

--- test/src/test_prog.c
+++ test/src/test_prog.c
@@ -7,7 +7,8 @@
 int main(int argc, char **argv)
 {
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <test_program>\n", argv[0]);
+        char *prog_name = argv[0][0] == '\0' ? "test_prog" : argv[0];
+        fprintf(stderr, "usage: %s <test_program>\n", prog_name);
         return 1;
     }
     hhg_unit_assert(!hhg_run(argv[1]));
