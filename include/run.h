b33aea7884ff9dc81b801153b1db8a3bf91b5308
unknown <odoul28@gmail.com>
2026-01-19 08:03:34 -0800

    Add run module

---
+++ include/run.h
@@ -0,0 +1,6 @@
+#ifndef HHG_RUN_H
+#define HHG_RUN_H
+
+void hhg_run(const char *filename);
+
+#endif
---
+++ src/run.c
@@ -0,0 +1,7 @@
+#include "run.h"
+#include "utils.h"
+
+void hhg_run(const char *filename)
+{
+    HHG_UNUSED(filename);
+}
